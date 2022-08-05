3968e360a034ca7433b17605ba9bfc0b5c267482
alimarcanotovar <aliymarcanotovar91@gmail.com>
2022-08-05 13:28:13 -0500

    variables

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String name = "Ali";
+            int myAge = 31;
+            char myCharacter = 'a';
+            bool isBool = true;
+            double myDouble = 5.0;
+            decimal myDecimal = -1m;
+            Console.WriteLine( $" My name is {name} I am {myAge} years old my name starts with {myCharacter} that is {isBool} {myDouble} {myDecimal}");
+
+
+
+        }
+    }
+}
